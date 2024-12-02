b35d0f7aa5fc08418b05169978e9343cb7df2174
parthibanb224 <parthiban.b@urbanrise.in>
2024-12-02 04:44:19 +0000

    commit111

--- src/pages/new/components/Walkin.js
+++ src/pages/new/components/Walkin.js
@@ -2,10 +2,10 @@ import { Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
 import gsap from "gsap";
 import Image from "next/image";
 import React, { useEffect, useRef } from "react";
-import Img1 from "../../../../public/Images-new/4-1-768x559.jpg";
-import Img2 from "../../../../public/Images-new/Co-working-Spaces-1-768x432.jpg";
-import Img3 from "../../../../public/Images-new/2-2-768x559.jpg";
-import Img4 from "../../../../public/Images-new/1-4-768x559.jpg";
+// import Img1 from "../../../../public/Images-new/4-1-768x559.jpg";
+// import Img2 from "../../../../public/Images-new/Co-working-Spaces-1-768x432.jpg";
+// import Img3 from "../../../../public/Images-new/tower-to-tower.jpg";
+// import Img4 from "../../../../public/Images-new/1-4-768x559.jpg";
 
 if (typeof window !== "undefined") {
   import("gsap/ScrollTrigger").then((ScrollTrigger) => {
@@ -62,28 +62,28 @@ export default function Walkin() {
 
   const sections = [
     {
-      imgSrc: Img1,
+      imgSrc: "/Images-new/4-1-768x559.jpg",
       title: "45K SQFT COSMO CLUBHOUSE",
       content:
         "An exclusive world-class club house spread over 45,000 Sq. Ft. of land invites you to the world of luxury beyond imagination. The magnificent area with contemporary design, upscale decor and latest indoor amenities is the perfect gateway to a life of fun and joy. The wide range of facilities in the club house ranges from beautiful spacious party hall to a world of indoor games like Table Tennis, Chess, and more.",
       reverse: false,
     },
     {
-      imgSrc: Img2,
+      imgSrc: "/Images-new/Co-working-Spaces-1-768x432.jpg",
       title: "15K SQFT OF CO-WORKING SPACES",
       content:
         "Experience a new dimension to ‘Working From Home’ at the sprawling Co-working spaces spread over 15,000 Sq.ft. Choose to work from the relaxing green environment in the beautiful landscaped park or scale new heights in your career working from the rooftop co-working spaces. Tailored to meet modern work needs, these co-working spaces balance work with relaxation, fun, and joy, transforming the typical work-from-home scenario into a more enjoyable, stress-free experience.",
       reverse: true,
     },
     {
-      imgSrc: Img3,
+      imgSrc: "/Images-new/tower-to-tower.jpg",
       title: "24K SQFT TOWER TO TOWER AMENITIES",
       content:
         "Innovation in every aspect of living is what The World of Joy offers you. Enjoy a wide range of games and activities like Shuttle Court, Snake Ladder & Chess Court laid out for you in the area between the towers. A smart usage of space extends beyond the walls of your home, the thoughtful planning adds a new dimension to living here.",
       reverse: false,
     },
     {
-      imgSrc: Img4,
+      imgSrc: "/Images-new/1-4-768x559.jpg",
       title: "1+ ACRES GREEN PARK",
       content:
         "Embrace a life close to nature in the World of Joy. An exquisite park spread over 1 acre of land consisting of beautiful green manicured lawns, pathways, play areas and professional play ground invites you to lead an active, healthy and wholesome life.",
